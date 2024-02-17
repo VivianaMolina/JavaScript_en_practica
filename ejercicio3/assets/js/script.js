26338a1724d40b25c9d4845b29634c1a359f93c8
Viviana Molina <viviana.a.molina@gmail.com>
2024-02-17 20:25:50 -0300

    added regular expression to accept only numbers as input

--- ejercicio3/assets/js/script.js
+++ ejercicio3/assets/js/script.js
@@ -6,6 +6,25 @@ var btnRestar= document.getElementById("btn-restar");
 var num1 = document.querySelector('#valor1');
 var num2 = document.querySelector('#valor2');
 
+//Valida que se ingresen sólo números 
+let sumar = document.getElementById("btn-sumar");
+sumar.addEventListener('click', validar);
+
+let restar = document.getElementById("btn-restar");
+restar.addEventListener('click', validar);
+
+function validar() {
+    let primerValor = document.querySelector("#valor1").value;
+    let SegundoValor = document.querySelector("#valor2").value;
+    let permitido = /[0-9]/gim;
+    if (primerValor.match(permitido) && SegundoValor.match(permitido)) {
+        Continue;
+    } else {
+        alert("Debe ingresar sólo números");
+    };
+};
+//Fin de Valida que se ingresen sólo números
+
 btnSumar.addEventListener('click',function(){
     Sumar(num1.value, num2.value);
     document.querySelector(".resultado").innerHTML = resultado;
